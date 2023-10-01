693beb800fc4956414c072149de08938fef83bcf
Milan Vaško <m.vasko2@gmail.com>
2023-10-01 15:55:48 +0200

    Add voiceovers

--- book_preview_panel/book_preview_panel.gd
+++ book_preview_panel/book_preview_panel.gd
@@ -8,6 +8,9 @@ var book_preview_scene := preload("res://book_preview/book_preview.tscn")
 
 func _on_found_book_index_updated() -> void:
 	var instance := book_preview_scene.instantiate()
-	instance.self_destruct = self_destruct_instances
 	instance.set_text(book_text_keeper.fetch_latest_text())
 	add_child(instance)
+
+func _on_voiceover_finished() -> void:
+	if self_destruct_instances:
+		get_child(0).queue_free()
---
+++ voiceover/voiceover.gd
@@ -0,0 +1,25 @@
+extends AudioStreamPlayer3D
+
+var current_index := -1
+
+@export var audio_files: Array[AudioStreamWAV]
+var queued_audio_files: Array[AudioStreamWAV] = []
+
+func _process(_delta: float) -> void:
+	if !playing && !queued_audio_files.is_empty():
+		var queued_audio_file := queued_audio_files[0]
+		queued_audio_files.remove_at(0)
+		stream = queued_audio_file
+		var bus_index := AudioServer.get_bus_index("EverythingExceptVoiceover")
+		AudioServer.set_bus_volume_db(bus_index, -10.0)
+		play()
+
+func _on_book_found() -> void:
+	current_index += 1
+	queued_audio_files.push_back(audio_files[current_index])
+	# get_tree().call_group("book_subscriber", "_on_found_book_index_updated")
+
+func _on_finished() -> void:
+	var bus_index := AudioServer.get_bus_index("EverythingExceptVoiceover")
+	AudioServer.set_bus_volume_db(bus_index, 0.0)
+	get_tree().call_group("book_subscriber", "_on_voiceover_finished")
