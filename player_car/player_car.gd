c2f38d86a81683e3d358bf7d070f278a04656a5d
Milan Vaško <m.vasko2@gmail.com>
2023-10-01 11:51:41 +0200

    Improve materials and lighting

--- player_car/player_car.gd
+++ player_car/player_car.gd
@@ -1,7 +1,10 @@
 extends VehicleBody3D
 
-@onready var score_keeper = $ScoreKeeper
-@onready var fuel_tank = $FuelTank
+@onready var score_keeper := $ScoreKeeper
+@onready var fuel_tank := $FuelTank
+
+@onready var back_light_left := $BackLightLeft
+@onready var back_light_right := $BackLightRight
 
 const MAX_ENGINE_FORCE := 400.0
 const MAX_BRAKE_FORCE := 5.0
@@ -9,6 +12,11 @@ const MAX_STEER_ANGLE := 0.6
 
 const STEER_SPEED := 5.0
 var steer_angle := 0.0
+
+func _process(_delta: float) -> void:
+	var braking := is_braking()
+	back_light_left.visible = braking
+	back_light_right.visible = braking
 
 func _physics_process(delta: float) -> void:
 	var throttle_amount := Input.get_action_strength("forward") - Input.get_action_strength("backward")
@@ -27,6 +35,9 @@ func _physics_process(delta: float) -> void:
 	elif steer_angle < steer_target:
 		steer_angle = min(steer_angle + steer_delta, steer_target)
 	steering = steer_angle
+
+func is_braking() -> bool:
+	return brake > 0.0 || is_braking_via_throttle()
 
 func is_braking_via_throttle() -> bool:
 	return linear_velocity.normalized().dot(transform.basis.z.normalized()) > 0.5 && engine_force < 0.0
